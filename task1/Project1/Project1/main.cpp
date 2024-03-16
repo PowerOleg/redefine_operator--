e6552010254bfc8ec5474190b89d767c46f381e7
PowerOleg <geraskin.oleg@mail.ru>
2024-03-16 15:35:44 +0300

    task1 is done

--- task1/Project1/Project1/main.cpp
+++ task1/Project1/Project1/main.cpp
@@ -17,19 +17,43 @@ public:
 	{
 		return this->numerator_ == right.numerator_ && this->denominator_ == right.denominator_;
 	}
+
+	bool operator!=(Fraction right)
+	{
+		return !(this->numerator_ == right.numerator_ && this->denominator_ == right.denominator_);
+	}
+
+	bool operator<(Fraction right)
+	{
+		return static_cast<double>(this->numerator_) / static_cast<double>(this->denominator_) < static_cast<double>(right.numerator_) / static_cast<double>(right.denominator_);
+	}
+
+	bool operator>(Fraction right)
+	{
+		return static_cast<double>(this->numerator_) / static_cast<double>(this->denominator_) > static_cast<double>(right.numerator_) / static_cast<double>(right.denominator_);
+	}
+
+	bool operator<=(Fraction right)
+	{
+		return static_cast<double>(this->numerator_) / static_cast<double>(this->denominator_) <= static_cast<double>(right.numerator_) / static_cast<double>(right.denominator_);
+	}
+
+	bool operator>=(Fraction right)
+	{
+		return static_cast<double>(this->numerator_) / static_cast<double>(this->denominator_) >= static_cast<double>(right.numerator_) / static_cast<double>(right.denominator_);
+	}
 };
 
 int main(int argc, char** argv)
 {
 	Fraction f1(4, 3);
 	Fraction f2(6, 11);
-	//Fraction f2(4, 3);
 
 	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
-	//std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
-	//std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
-	//std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
-	//std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
-	//std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
 	return 0;
 }
